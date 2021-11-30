3e15fd1c03c78131b287feb7e3607c6222166cab
Ivan <smolyanov.iv@gmail.com>
2021-11-30 21:29:57 +0300

    third commit

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ require('dotenv').config()
 const bot = new Telegraf(process.env.BOT_TOKEN)
 bot.start((ctx) => ctx.reply(`Hello, ${ctx.message.from.first_name ? ctx.message.from.first_name : ""} ${ctx.message.from.last_name ? ctx.message.from.last_name : ""}!`))
 bot.hears('Hello there', (ctx) => ctx.reply("General Kenoby!"))
-bot.on('message', (ctx) => ctx.reply(ctx.message.text))
+bot.on('message', (ctx) => {
+    if(ctx.message.text) {
+        ctx.reply(ctx.message.text)
+    } else if(ctx.message.dice) {
+        ctx.reply(ctx.message.dice.emoji)
+    }
+    
+})
 bot.launch()
 
 // Enable graceful stop
