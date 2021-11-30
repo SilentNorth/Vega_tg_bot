5e0b9f970da55070fb6168f19c9ae1e5ddc8a045
Ivan <smolyanov.iv@gmail.com>
2021-12-01 01:32:23 +0300

    fourth commit

--- index.js
+++ index.js
@@ -5,20 +5,8 @@ const bot = new Telegraf(process.env.BOT_TOKEN);
 
 bot.start((ctx) => ctx.reply(`Hello, ${ctx.message.from.first_name ? ctx.message.from.first_name : ""} ${ctx.message.from.last_name ? ctx.message.from.last_name : ""}!`));
 
-bot.on('message', (ctx) => {
-    if(ctx.message.text) {
+bot.on('message', (ctx) => ctx.telegram.copyMessage(ctx.message.chat.id, ctx.message.chat.id, ctx.message.message_id));
 
-        ctx.reply(ctx.message.text)
-
-    } else if(ctx.message.dice) {
-
-        ctx.telegram.sendDice(ctx.message.chat.id, {emoji: ctx.message.dice.emoji} );
-
-    } else if(ctx.message.sticker) {
-        ctx.telegram.sendSticker(ctx.message.chat.id, ctx.message.sticker.file_id);
-    }
-    
-})
 bot.launch();
 
 // Enable graceful stop
