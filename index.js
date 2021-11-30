ea7131d0a8d6d0f1ca0b0596d5ab9f470e455a81
Ivan <smolyanov.iv@gmail.com>
2021-11-30 22:06:41 +0300

    third commit

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-const { Telegraf } = require('telegraf')
-require('dotenv').config()
+function randomInteger(min, max) {    
+    let rand = min - 0.5 + Math.random() * (max - min + 1);
+    return Math.round(rand);
+  }
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start((ctx) => ctx.reply(`Hello, ${ctx.message.from.first_name ? ctx.message.from.first_name : ""} ${ctx.message.from.last_name ? ctx.message.from.last_name : ""}!`))
-bot.hears('Hello there', (ctx) => ctx.reply("General Kenoby!"))
+const { Telegraf } = require('telegraf');
+require('dotenv').config();
+
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+bot.start((ctx) => ctx.reply(`Hello, ${ctx.message.from.first_name ? ctx.message.from.first_name : ""} ${ctx.message.from.last_name ? ctx.message.from.last_name : ""}!`));
+
 bot.on('message', (ctx) => {
     if(ctx.message.text) {
         ctx.reply(ctx.message.text)
     } else if(ctx.message.dice) {
-        ctx.reply(ctx.message.dice)
+        if(ctx.message.dice.emoji == "🎰"){
+            app.telegram.sendDice(ctx.message.chat.id, "🎰");
+        }
+        // ctx.reply(ctx.message.dice)
     }
     
 })
-bot.launch()
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
